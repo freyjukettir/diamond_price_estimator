ec6fbdd1a27730e2428d90fd6b0161bf6fcf98ae
freyjukettir <clb@lynxcoatingsresearch.com>
2026-05-22 12:05:02 -0500

    Add files via upload

--- ui.R
+++ ui.R
@@ -16,6 +16,12 @@ fluidPage(
     # Sidebar with controls for parameters
     sidebarLayout(
         sidebarPanel(
+            helpText('Enter the carat weight of the diamond in question.', br(),
+            'Then select the color, cut, and clarity.', br(),
+            'An estimated price for the diamond will be provided,', br(),
+            'and the estimate plotted (intersection of the red lines) with the', br(),
+            'data points used to develop the price estimation model.'),
+            br(), br(),
             numericInput('carat_in',
                          label = 'Weight (carats)', 
                          value = 1,
